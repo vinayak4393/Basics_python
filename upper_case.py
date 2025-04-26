5af917b1522192ab4b01de9962bdc91661236891
vinayak4393 <vinaykwod4393@gmail.com>
2025-04-26 16:23:43 +0530

    function_for_uppercase 26-April-25 04:23PM

---
+++ upper_case.py
@@ -0,0 +1,13 @@
+# Write a function which converts the input string to uppercase.
+
+def upperCase(str1):
+    return str1.upper()
+
+str2 = "Machine learning"
+out = upperCase(str2)
+print(out)
+
+
+
+
+
